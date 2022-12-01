a89e45e6e9356d1cb6308deae6893a7e3be70bd2
Сергей Балуков <balukov.s@roistat.com>
2022-12-01 12:27:23 +0300

    I forgor vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'vkcom/vk-php-sdk' => array(
+            'pretty_version' => '5.101.0',
+            'version' => '5.101.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../vkcom/vk-php-sdk',
+            'aliases' => array(),
+            'reference' => '46a8f76503050c0c6730359fc234a4d6448b8653',
+            'dev_requirement' => false,
+        ),
+    ),
+);
